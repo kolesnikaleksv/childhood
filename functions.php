c71cff6df1502052b8c54d05f3b2a56f3190c6c9
kolesnikaleksv <kolesnikaleksv@gmail.com>
2024-06-06 17:53:42 +0300

    Add optimized photo and custom logo

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
     // wp_enqueue_style('header-style', get_template_directory_uri() . '/assets/styles/main.min.css'); // connect another styles
     // wp_enqueue_style('animate-style', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'); // connect styles from cdn
     wp_enqueue_script('childhood-script', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
-  }
+  };
 
-
+  add_theme_support( 'custom-logo');
 ?>
